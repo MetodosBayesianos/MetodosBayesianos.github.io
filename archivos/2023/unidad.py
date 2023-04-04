9dd3fa972a96edc2570fa3dffa26154451324104
Gustavo Landfried <gustavolandfried@gmail.com>
2023-04-03 21:13:36 -0300

    Datos y estructura

---
+++ archivos/2023/unidad.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+datos = pd.read_csv("regalos.csv")
+
+def creencia(datos):
+    # Escribir
+
+def elegir_caja(tiempo):
+    # Escribir
+
+def apostar_y_repartir(pista,tiempo):
+    # Escribir
